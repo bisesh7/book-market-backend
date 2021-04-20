93e0cdcb605336470a4f1971b088acb118617523
Bisesh Shakya <bisesh.shakya@gmail.com>
2021-04-20 10:30:52 +0545

    Created api key and used apit key in request response

--- Routes/API/books.js
+++ Routes/API/books.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const books = require("../../data/book_set.json");
+const config = require("config");
 
 router.get("/", (req, res) => {
+  const API_KEY = req.headers.authorization;
+
+  if (API_KEY !== config.get("API_KEY")) {
+    return res.status(403).json({ success: false, msg: "Unauthorized access" });
+  }
+
   return res.json({ success: true, books });
 });
 
