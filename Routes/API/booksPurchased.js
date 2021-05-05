4139e1eab37846846f7de21d3c0d9501c7e26f5c
Bisesh Shakya <bisesh.shakya@gmail.com>
2021-05-05 11:13:40 +0545

    Used async await instead of promise in Book.find

--- Routes/API/booksPurchased.js
+++ Routes/API/booksPurchased.js
@@ -16,7 +16,7 @@ const async = require("async");
 // @route   POST /api/purchase_book/
 // @desc    Adds the purchase history to the db
 // @access  private
-router.post("/", [checkAPIKey, checkAccessRights], (req, res) => {
+router.post("/", [checkAPIKey, checkAccessRights], async (req, res) => {
   const {
     purchasedBooks,
     usedCoupon,
@@ -55,101 +55,102 @@ router.post("/", [checkAPIKey, checkAccessRights], (req, res) => {
       err: bookError,
     });
   }
+
   // Get the books purchases from the database
-  Book.find({ id: { $in: purchasedBookIds } }).then((books) => {
-    // Book purchased validation
-    if (!books.length) {
-      return res.status(400).json({
-        success: false,
-        msg: "Books with given book id were not found",
-        err: bookError,
-      });
+  const books = await Book.find({ id: { $in: purchasedBookIds } });
+
+  // Book purchased validation
+  if (!books.length) {
+    return res.status(400).json({
+      success: false,
+      msg: "Books with given book id were not found",
+      err: bookError,
+    });
+  }
+
+  for (let i = 0; i < books.length; i++) {
+    const book = books[i];
+    // If the stock of book is 0 then client has errors
+    if (book.stock <= 0) {
+      setPurchaseValidation(
+        false,
+        `Stock of ${book["name "]} is 0`,
+        stockError
+      );
+      break;
     }
+    for (let j = 0; j < purchasedBooks.length; j++) {
+      const bookPurchased = purchasedBooks[j];
+      // If the stock - quantity is less than 0 in one of the books purchased, client has errors
+      if (book.id === bookPurchased.bookId) {
+        if (book.stock - bookPurchased.quantity < 0) {
+          setPurchaseValidation(
+            false,
+            `Only ${book.stock} ${book["name "]} is left.`,
+            stockError
+          );
+          break;
+        }
 
-    for (let i = 0; i < books.length; i++) {
-      const book = books[i];
-      // If the stock of book is 0 then client has errors
-      if (book.stock <= 0) {
-        setPurchaseValidation(
-          false,
-          `Stock of ${book["name "]} is 0`,
-          stockError
-        );
-        break;
-      }
-      for (let j = 0; j < purchasedBooks.length; j++) {
-        const bookPurchased = purchasedBooks[j];
-        // If the stock - quantity is less than 0 in one of the books purchased, client has errors
-        if (book.id === bookPurchased.bookId) {
-          if (book.stock - bookPurchased.quantity < 0) {
-            setPurchaseValidation(
-              false,
-              `Only ${book.stock} ${book["name "]} is left.`,
-              stockError
-            );
-            break;
-          }
-
-          // Checking the amount calculated in the client
-          if (
-            parseFloat(book.price.slice(1)) * bookPurchased.quantity !==
-            bookPurchased.amount
-          ) {
-            setPurchaseValidation(
-              false,
-              `Amount calculated is not correct for ${book["name "]}.`,
-              amountError
-            );
-            break;
-          }
+        // Checking the amount calculated in the client
+        if (
+          parseFloat(book.price.slice(1)) * bookPurchased.quantity !==
+          bookPurchased.amount
+        ) {
+          setPurchaseValidation(
+            false,
+            `Amount calculated is not correct for ${book["name "]}.`,
+            amountError
+          );
+          break;
         }
       }
     }
+  }
 
-    if (!purchaseValidation.valid) {
-      return res.status(400).json({
-        success: false,
-        msg: purchaseValidation.msg,
-        err: purchaseValidation.err,
-      });
-    }
+  if (!purchaseValidation.valid) {
+    return res.status(400).json({
+      success: false,
+      msg: purchaseValidation.msg,
+      err: purchaseValidation.err,
+    });
+  }
 
-    const savePromises = [];
+  const savePromises = [];
 
-    for (let i = 0; i < books.length; i++) {
-      const book = books[i];
+  for (let i = 0; i < books.length; i++) {
+    const book = books[i];
 
-      for (let j = 0; j < purchasedBooks.length; j++) {
-        const bookPurchased = purchasedBooks[j];
-        if (book.id === bookPurchased.bookId) {
-          book.stock -= bookPurchased.quantity;
-          savePromises.push((cb) => {
-            book.save().then((result) => {
-              cb(null, result);
-            });
+    for (let j = 0; j < purchasedBooks.length; j++) {
+      const bookPurchased = purchasedBooks[j];
+      if (book.id === bookPurchased.bookId) {
+        book.stock -= bookPurchased.quantity;
+        savePromises.push((cb) => {
+          book.save().then(() => {
+            cb(null, i);
           });
-        }
+        });
       }
     }
+  }
 
-    async.parallel(savePromises).then((result) => {
-      if (result.length !== books.length) {
-        // err
-      }
-      console.log(result);
-    });
-
-    const newUserBooksPurchase = new UserBooksPurchase({
-      userId,
-      purchasedBooks,
-      usedCoupon,
-      discount,
-      subTotalAmount,
-      totalAmount,
-    });
-
-    return res.json({ success: true });
+  async.parallel(savePromises).then((result) => {
+    if (result.length !== books.length) {
+      // err
+    }
+    console.log(result);
   });
+
+  const newUserBooksPurchase = new UserBooksPurchase({
+    userId,
+    purchasedBooks,
+    usedCoupon,
+    discount,
+    subTotalAmount,
+    totalAmount,
+  });
+
+  return res.json({ success: true });
 
   //   newUserBooksPurchase
   //     .save()
