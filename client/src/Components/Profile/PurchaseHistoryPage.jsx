516905c327a94522ead70cf90743eee83e75f676
Bisesh Shakya <bisesh.shakya@gmail.com>
2021-05-07 16:24:58 +0545

    Removed toolkit

--- client/src/Components/Profile/PurchaseHistoryPage.jsx
+++ client/src/Components/Profile/PurchaseHistoryPage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import BootstrapTable from "react-bootstrap-table-next";
-import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
 import { getNPRFromDollar } from "../../utils/getNPRFromDollar";
 import { getPurchasedBooks } from "../../config/authAPI";
 import { connect } from "react-redux";
@@ -9,7 +8,7 @@ import { getFormattedDate } from "../../utils/getFormattedDate";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { setBooks } from "../../Actions/actionBook";
-import paginationFactory from "react-bootstrap-table2-paginator";
+// import paginationFactory from "react-bootstrap-table2-paginator";
 
 const imageFormatter = (cell, row) => {
   return <img className="purchase-history-table-img" src={cell} alt={cell} />;
@@ -105,28 +104,26 @@ const PurchaseHistoryPage = (props) => {
     // eslint-disable-next-line
   }, [props.setBooks]);
 
-  const { SearchBar } = Search;
-
   const customTotal = (from, to, size) => (
     <span className="react-bootstrap-table-pagination-total">
       Showing {from} to {to} of {size} Results
     </span>
   );
 
-  const paginationOptions = {
-    pageStartIndex: 0,
-    sizePerPage: 5,
-    hideSizePerPage: true,
-    hidePageListOnlyOnePage: true,
-    paginationSize: 3,
-    firstPageText: "First",
-    prePageText: "Back",
-    nextPageText: "Next",
-    lastPageText: "Last",
-    disablePageTitle: true,
-    showTotal: true,
-    paginationTotalRenderer: customTotal,
-  };
+  // const paginationOptions = {
+  //   pageStartIndex: 0,
+  //   sizePerPage: 5,
+  //   hideSizePerPage: true,
+  //   hidePageListOnlyOnePage: true,
+  //   paginationSize: 3,
+  //   firstPageText: "First",
+  //   prePageText: "Back",
+  //   nextPageText: "Next",
+  //   lastPageText: "Last",
+  //   disablePageTitle: true,
+  //   showTotal: true,
+  //   paginationTotalRenderer: customTotal,
+  // };
 
   const [products, setProducts] = useState([]);
 
@@ -174,33 +171,25 @@ const PurchaseHistoryPage = (props) => {
 
   return (
     <div className="purchase-history-table-container">
-      <ToolkitProvider keyField="id" data={products} columns={columns} search>
-        {(props) => (
-          <div>
-            <SearchBar {...props.searchProps} />
-            <hr />
-            <BootstrapTable
-              classes="purchase-history-table shadow mt-2"
-              keyField="id"
-              data={products}
-              columns={columns}
-              defaultSorted={defaultSorted}
-              bootstrap4
-              noDataIndication={
-                loading ? (
-                  <span>
-                    <FontAwesomeIcon icon={faSpinner} spin />
-                  </span>
-                ) : (
-                  "No purchase history available."
-                )
-              }
-              // pagination={paginationFactory()}
-              {...props.baseProps}
-            />
-          </div>
-        )}
-      </ToolkitProvider>
+      <BootstrapTable
+        classes="purchase-history-table shadow mt-2"
+        keyField="id"
+        data={products}
+        columns={columns}
+        defaultSorted={defaultSorted}
+        bootstrap4
+        noDataIndication={
+          loading ? (
+            <span>
+              <FontAwesomeIcon icon={faSpinner} spin />
+            </span>
+          ) : (
+            "No purchase history available."
+          )
+        }
+        // pagination={paginationFactory()}
+        {...props.baseProps}
+      />
     </div>
   );
 };
